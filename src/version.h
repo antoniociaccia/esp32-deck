a17b5e0252765d7d26830e2f4621ae2047b01cd4
Antonio Ciaccia <46628027+antoniociaccia@users.noreply.github.com>
2026-03-24 19:20:51 +0100

    chore: bump firmware version to 0.1.1 in `version.h`

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-static constexpr const char *FW_VERSION = "0.1.0";
+static constexpr const char *FW_VERSION = "0.1.1";
 static constexpr const char *FW_RELEASE_CHANNEL = "stable";
 static constexpr const char *FW_BOARD_ID = "esp32s3";
 
